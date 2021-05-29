e3cdf45fa3c29c05452b887ca11fa14008419dce
Ezihe Godswill <ezihegodswill01@gmail.com>
2021-05-29 05:49:40 +0100

    fixed appendChild not working issue

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
 const searchName = JSON.parse(localStorage.getItem("username"));
+let repoCardsContainer = document.querySelector('.secondary-col');
+let repoCard = document.createElement("div");
+repoCard.className = "github-repo-card";
 
 const findUser = async (userName) => {
     fetch(`https://api.github.com/users/${userName}`)
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => {
+            // console.log(data)
+            const { login, name, bio, avatar_url, followers, public_repos } = data;
+            const userBio = document.querySelector('.github-user-about').innerHTML = bio;
+            const userLogin = document.querySelector('.github-name').textContent = login;
+            const usercName = document.querySelector('.github-username').textContent = name;
+            const userRepoCount = document.querySelector('.github-repo-no').textContent = public_repos;
+        }
+        )
         // .then(localStorage.clear())
         .catch(error => alert(error))
 }
@@ -11,7 +22,35 @@ const findUser = async (userName) => {
 const findUserRepos = async (userName) => {
     fetch(`https://api.github.com/users/${userName}/repos`)
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => {
+            for (let i = 0; i < 20; i++) {
+                console.log(data[i]);
+                repoCard.innerHTML = `<div class="github-repo-content">
+                <h4 class="github-repo-name">
+                  <a href="${data[i].html_url}" class="github-repo-name-link"
+                    >${data[i].name}</a
+                  >
+                </h4>
+                <div class="github-repo-stats">
+                  <div class="github-repo-stat-box">
+                    <span class="fas fa-circle"></span>&nbsp;HTML
+                  </div>
+                  <div class="github-repo-stat-box">
+                    <span class="far fa-star"></span>&nbsp;${data[i].stargazers_count}
+                  </div>
+                  <div class="github-repo-stat-box">
+                    <span class="github-repo-last-updated"
+                      >Updated 12 days ago</span
+                    >
+                  </div>
+                </div>
+              </div>
+              <button class="github-repo-star">
+                <span class="fas fa-star"></span> Star
+              </button>`;
+                repoCardsContainer.appendChild(repoCard.cloneNode(true));
+            }
+        })
         .catch(error => console.error(error))
 }
 
--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 const form = document.querySelector('.home-form');
+const searchBox = document.querySelector('#home-search').value;
+const searchValue = searchBox.split(' ').join('');
 
 const handleSubmit = (e) => {
     e.preventDefault();
@@ -6,6 +8,45 @@ const handleSubmit = (e) => {
     const searchValue = searchBox.split(' ').join('');
     localStorage.setItem("username", JSON.stringify(searchValue));
     window.location.href = '../profile.html';
+    searchBox = document.querySelector('#home-search').value = '';
 }
 
 form.addEventListener('submit', handleSubmit)
+// form.addEventListener('submit', handleSubmit)
+
+// function handleSubmit(e) {
+//     e.preventDefault();
+//     const searchBox = document.querySelector('#home-search').value;
+//     const searchValue = searchBox.split(' ').join('');
+
+//     localStorage.setItem("username", JSON.stringify(searchValue));
+//     const searchName = JSON.parse(localStorage.getItem("username"));
+
+//     findUser(searchName);
+//     findUserRepos(searchName);
+
+//     const changePage = async () => window.location.href = '../profile.html';
+//     changePage();
+
+// }
+
+// const findUser = async (userName) => {
+//     fetch(`https://api.github.com/users/${userName}`)
+//         .then(response => response.json())
+//         .then(data => console.log(data))
+//         // .then(localStorage.clear())
+//         .catch(error => alert(error))
+// }
+
+// const findUserRepos = async (userName) => {
+//     fetch(`https://api.github.com/users/${userName}/repos`)
+//         .then(response => response.json())
+//         .then(data => console.log(data))
+//         .catch(error => console.error(error))
+// }
+
+// const findUser = async (e) => {
+//     const response = await fetch(`https://api.github.com/users/${searchValue}`);
+//     const responseJson = await response.json();
+//     console.log(responseJson);
+// }
