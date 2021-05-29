ec291d5015862e3e3a94b86120e8b649490061ea
Ezihe Godswill <ezihegodswill01@gmail.com>
2021-05-29 02:10:41 +0100

    form submission issue fixes in progress

--- js/index.js
+++ js/index.js
@@ -1,14 +1,40 @@
 const form = document.querySelector('.home-form');
 
-form.addEventListener('submit', (e) => {
-    // e.preventDefault();
-    window.location.href = '../profile.html';
+form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(e) {
+    e.preventDefault();
     const searchBox = document.querySelector('#home-search').value;
     const searchValue = searchBox.split(' ').join('');
-    // alert(searchValue);
-    fetch(`https://api.github.com/users/${searchValue}`)
-        .then(response => response.json)
+
+    localStorage.setItem("username", searchValue);
+    const searchName = localStorage.getItem("username");
+
+    findUser(searchName);
+    findUserRepos(searchName);
+
+    // const changePage = async () => window.location.href = '../profile.html';
+    // changePage();
+
+}
+
+const findUser = async (userName) => {
+    fetch(`https://api.github.com/users/${userName}`)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        // .then(localStorage.clear())
+        .catch(error => console.error(error))
+}
+
+const findUserRepos = async (userName) => {
+    fetch(`https://api.github.com/users/${userName}/repos`)
+        .then(response => response.json())
         .then(data => console.log(data))
         .catch(error => console.error(error))
+}
 
-})
+// const findUser = async (e) => {
+//     const response = await fetch(`https://api.github.com/users/${searchValue}`);
+//     const responseJson = await response.json();
+//     console.log(responseJson);
+// }
